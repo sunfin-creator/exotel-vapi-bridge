5fe5fd59e146b8520b5c9889a0d8dabb7e202f74
sunfin-creator <sunfin@sunitaonline.com>
2026-06-12 12:53:50 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Exotel Vapi Bridge Running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
